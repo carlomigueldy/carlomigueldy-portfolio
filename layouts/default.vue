4bdbeb66c06eadd02981d667267c278abeaff87d
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-09 10:51:29 +0800

    added wallpaper

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,20 @@
       elevate-on-scroll
       inverted-scroll
       fixed
-      dense></v-app-bar>
+      dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Carlo Miguel Dy</v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+      <v-btn>About</v-btn>
+    </v-app-bar>
     
     <v-content>
       <nuxt />
     </v-content>
 
     <v-btn 
+      @click="$vuetify.goTo('#landing')"
       fixed 
       bottom 
       right
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,50 @@
 <template>
   <div>
-    <hero-component class="text-center">
-      <v-row 
-        justify="center"
-        align="center">
-        <v-col>
-          <section class="text-center">
-            <div class="carlomigueldy">{ Dy }</div>
-            <div class="text-center">
-              <div>I am a full stack developer based in the Philippines,</div>
-              <div>primarily using Laravel for building APIs and </div>
-              <div>Nuxt js for the frontend.</div>
-            </div>
-          </section>
-        </v-col>
-      </v-row>
-    </hero-component>
+    <v-img 
+      max-height="100vh" 
+      src="/wallpaper.jpg"
+      class="parallax"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.95)">
+      <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center">
+          <v-progress-circular 
+            indeterminate 
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+      <hero-component id="landing" class="text-center">
+        <v-row 
+          justify="center"
+          align="center">
+          <v-col>
+            <section class="text-center">
+              <div class="carlomigueldy">{ Dy }</div>
+              <div class="text-center subtitle-2">
+                <p>
+                  <div>I am a full stack developer based in the Philippines,</div>
+                  <div>primarily using Laravel for building APIs and </div>
+                  <div>Nuxt js for the frontend.</div>
+                </p>
+                <v-btn 
+                  large 
+                  rounded
+                  color="deep-purple darken-4"
+                  @click="$vuetify.goTo('#projects')">
+                  See Projects
+                  <v-icon right>mdi-chevron-down</v-icon>
+                </v-btn>
+              </div>
+            </section>
+          </v-col>
+        </v-row>
+      </hero-component>
+    </v-img>
 
-    <hero-component>
+    <hero-component id="projects">
       Hello World
     </hero-component>
   </div>
@@ -39,11 +66,18 @@ export default {
 .carlomigueldy {
   font-family: 'Noto Sans JP', sans-serif;
   font-size: 15vh;
-  font-weight: 100;
+  font-weight: 300;
 }
 
 .mx-20 {
   margin-left: 14rem;
   margin-right: 14rem;
+}
+
+.parallax {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
