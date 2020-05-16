3af2da07dffcd829e1809843545a0e988d0358bc
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-05-16 20:06:21 +0800

    added image on projects

--- components/ProjectComponent.vue
+++ components/ProjectComponent.vue
@@ -14,6 +14,21 @@
           <v-card 
             @click="goTo(item.link)" 
             class="elevation-2">
+            <v-card-text>
+              <v-img :src="item.img">
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center">
+                    <v-progress-circular 
+                      indeterminate 
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-card-text>
             <v-card-title 
               class="headline"
               v-text="item.title"
@@ -74,6 +89,21 @@
                     color="primary"
                     @click="goTo(item.link)" 
                     class="elevation-2">
+                    <v-card-text>
+                      <v-img :src="item.img">
+                        <template v-slot:placeholder>
+                          <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center">
+                            <v-progress-circular 
+                              indeterminate 
+                              color="grey lighten-5"
+                            ></v-progress-circular>
+                          </v-row>
+                        </template>
+                      </v-img>
+                    </v-card-text>
                     <v-card-title 
                       class="headline"
                       v-text="item.title"
@@ -129,6 +159,7 @@ export default {
         color: "blue",
         date: "April 2020",
         demo_available: true,
+        img: "/img/fpv-crm/laptop.png",
         link: "https://fpv-crm.netlify.app/",
         description: "A custom tailored CRM System on a decoupled architecture built with Laravel 6 as the API and Nuxt js with Vuetify on the frontend. The project has features like Payroll System, Clock-in & Clock-out System, Order System, Invoicing System, User Management System, Email Templating System, & a unique Reporting System.",
         tech_used: [
@@ -160,6 +191,7 @@ export default {
         color: "red",
         date: "November 2019",
         demo_available: true,
+        img: "/img/ethnogis/laptop.png",
         link: "http://ethnogis.herokuapp.com/",
         description: "An undergraduate Capstone project, a web-based application that focuses on displaying data-driven interactive maps. It delivers data management for maintaining information, provides data visualizations using interactive driven maps and the utilization of charts of the Indigenous Groups in Northern Mindanao (Region X), Philippines. IndiGIS is built with Laravel, Vue js, Vuetify and Leaflet js. And it also integrates with the Progressive Web Application (PWA) built on Vuetify and Vue js to achieve offline data gathering capabilities.",
         tech_used: [
@@ -189,6 +221,7 @@ export default {
         color: "green",
         date: "October 2019",
         demo_available: true,
+        img: "/img/dymotherhackers/laptop.png",
         link: "https://dymotherhackers.firebaseapp.com/",
         description: `A prototype produced from a Hackathon named "Clean Tech Hack", it is Progressive Web Application that uses Geographical Information System with Photogrammetry for 3D modelling. Developers and Engineers for Marawi city can easily inspect what's the current state of the area and see its progress of rebuildig Marawi. This Progressive Web Application (PWA) was built with Vue js & Vuetify.`,
         tech_used: [
@@ -212,6 +245,7 @@ export default {
         color: "purple",
         date: "October 2019",
         demo_available: true,
+        img: "/img/asean/laptop.png",
         link: "https://aseanmsuiit.firebaseapp.com/",
         description: "A simple blog on climate change prevention built with Vue js & Buefy/Bulma.",
         tech_used: [
@@ -231,6 +265,7 @@ export default {
         color: "red",
         date: "August 2019",
         demo_available: true,
+        img: "/img/gameofluck/laptop.png",
         link: "https://carlomigueldy.github.io/gameofluck/",
         description: "A simple game on player versus monster built with Vue js.",
         tech_used: [
