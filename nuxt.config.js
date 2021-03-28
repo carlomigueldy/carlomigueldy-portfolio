2a4b193022ee4d8e1f15fec50df594985df10832
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-28 16:00:53 +0800

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,12 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    titleTemplate: '%s - carlomigueldy-portfolio',
     title: 'carlomigueldy-portfolio',
     meta: [
       { charset: 'utf-8' },
@@ -30,7 +33,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-      'nuxt-windicss',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -43,6 +47,25 @@ export default {
   pwa: {
     manifest: {
       lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
     }
   },
 
