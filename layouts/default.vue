1c1633e20e752ca348b36762e1feed3582db0c38
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-28 20:35:50 +0800

    feat: new look

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,142 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+  <v-app app>
+    <v-app-bar :color="$vuetify.theme.dark ? '#022c43' : 'white'" app flat>
+      <v-toolbar-title>
+        Dy
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+        <v-btn text x-large>Blog</v-btn>
+        <v-btn text x-large>Projects</v-btn>
+        <v-btn
+          v-if="
+            $vuetify.breakpoint.md ||
+              $vuetify.breakpoint.lg ||
+              $vuetify.breakpoint.xl
+          "
+          href="https://twitter.com/CarloMiguelDy"
+          target="_blank"
+          elevation="0"
+          color="transparent"
+          fab
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn
+          v-if="
+            $vuetify.breakpoint.md ||
+              $vuetify.breakpoint.lg ||
+              $vuetify.breakpoint.xl
+          "
+          href="https://facebook.com/polarixdy"
+          target="_blank"
+          elevation="0"
+          color="transparent"
+          fab
+        >
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+        <v-btn
+          href="https://github.com/carlomigueldy"
+          target="_blank"
+          elevation="0"
+          color="transparent"
+          fab
+        >
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+        <v-btn
+          href="https://www.linkedin.com/in/carlo-miguel-dy-168797181/"
+          target="_blank"
+          elevation="0"
+          color="transparent"
+          fab
+        >
+          <v-icon>mdi-linkedin</v-icon>
+        </v-btn>
+
+        <v-btn
+          @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+          elevation="0"
+          color="transparent"
+          fab
+        >
+          <v-icon v-if="$vuetify.theme.dark">mdi-white-balance-sunny</v-icon>
+          <v-icon v-else>mdi-weather-night</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
+
+    <v-main
+      :style="{
+        'background-color': $vuetify.theme.dark ? '#022c43' : 'transparent'
+      }"
+      class="my-10"
+    >
+      <v-container fill-height>
+        <v-row justify="center" align="start" style="height: 100%;">
+          <v-col xs="12" sm="11" md="8" lg="7" xl="6" style="height: 100%;">
+            <v-row justify="center" align="start" no-gutters>
+              <v-col cols="12" xs="12" sm="12" md="4" lg="4" xl="4">
+                <div class="text-center">
+                  <v-avatar height="150" width="150">
+                    <v-img src="me.jpg"></v-img>
+                  </v-avatar>
+                </div>
+              </v-col>
+              <v-col cols="12" xs="12" sm="12" md="8" lg="8" xl="8">
+                <div
+                  :class="{
+                    'text-left':
+                      $vuetify.breakpoint.md ||
+                      $vuetify.breakpoint.lg ||
+                      $vuetify.breakpoint.xl,
+                    'text-center mt-5':
+                      $vuetify.breakpoint.sm || $vuetify.breakpoint.xs
+                  }"
+                >
+                  <h1 class="display-2">Carlo Miguel Dy</h1>
+                  <h2 class="my-3 body-1">
+                    Software Engineer · Aspiring Software Architect
+                  </h2>
+                  <a
+                    href="mailto:carlomigueldy@gmail.com"
+                    class="primary--text text-decoration-none"
+                  >
+                    <v-icon color="primary">mdi-email-outline</v-icon>
+                    <span>carlomigueldy@gmail.com</span>
+                  </a>
+                </div>
+              </v-col>
+            </v-row>
+
+            <!-- <div class="my-5 d-flex justify-center align-center">
+              <v-btn text x-large class="subtitle-1">About</v-btn>
+              <v-btn text x-large class="subtitle-1">Skills</v-btn>
+              <v-btn text x-large class="subtitle-1">Blog</v-btn>
+              <v-btn text x-large class="subtitle-1">Projects</v-btn>
+              <v-btn text x-large class="subtitle-1">Contact</v-btn>
+            </div> -->
+
+            <v-divider class="my-10"></v-divider>
+            <Nuxt />
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer :color="$vuetify.theme.dark ? '#022c43' : 'white'" app>
+      <v-container>
+        <div class="text-center">
+          Made with ❤️ by Carlo Miguel Dy
+        </div>
+      </v-container>
     </v-footer>
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+  computed: {}
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,25 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - carlomigueldy-portfolio',
-    title: 'carlomigueldy-portfolio',
+    title: "Carlo Miguel Dy",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ["~/plugins/vuetify"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,29 +27,32 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
+
+    "@nuxtjs/google-fonts"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa"
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: "en"
     }
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
+    treeShake: true,
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -69,7 +67,13 @@ export default {
     }
   },
 
+  googleFonts: {
+    useStylesheet: true,
+    families: {
+      Sora: true
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
